2f1fd709435ec45987482637242649ff88480f21
Strannik73 <Santon@tutamail.com>
2025-11-25 13:29:13 +0300

    csa

---
+++ script.js
@@ -0,0 +1,26 @@
+const alphabet = [
+'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К',
+'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц',
+'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я', ' ', 
+
+];
+
+const keyboardDiv = document.getElementById('game');
+const buttonsPerRow = 9; 
+
+let currentRow = null;
+
+for (let i = 0; i < alphabet.length; i++) {
+if (i % buttonsPerRow === 0) {
+    currentRow = document.createElement('div');
+    currentRow.className = 'keyboard-row';
+    keyboardDiv.appendChild(currentRow);
+}
+
+const button = document.createElement('button');
+
+button.textContent = alphabet[i];
+
+
+currentRow.appendChild(button);
+}
